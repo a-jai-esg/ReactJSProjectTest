b0eda54a110677b928e53d9da56d1f24622b5675
Elijah Nicholas Esguerra <ashesg1117@gmail.com>
2023-03-21 23:05:10 +0800

    Initial

---
+++ esguerra-react-project/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import gaw from 'img/gaw.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={gaw} className="App-logo" alt="logo" />
+        <p>Gaw onsa atoha gaw? HAHAHAHA<p/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
